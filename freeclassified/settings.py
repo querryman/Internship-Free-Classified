fed187af3483c2f68f5a97d5976debcecb0146e0
projectaccount007 <103994837+projectaccount007@users.noreply.github.com>
2025-04-07 16:51:46 +0530

    pagination

--- freeclassified/settings.py
+++ freeclassified/settings.py
@@ -62,6 +62,11 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
 ]
 
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10,
+}
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
