dba0551e9a5bc272bfcb3f7cbdf77bcadb6456a2
projectaccount007 <103994837+projectaccount007@users.noreply.github.com>
2025-04-07 14:37:30 +0530

    filtering logic for listings

--- listings/api_views.py
+++ listings/api_views.py
@@ -1,10 +1,15 @@
-from rest_framework import generics
+from rest_framework import generics, filters
+from django_filters.rest_framework import DjangoFilterBackend
 from .models import Listing
 from .serializers import ListingSerializer
 
 class ListingList(generics.ListCreateAPIView):
     queryset = Listing.objects.all().order_by('-created_at')
     serializer_class = ListingSerializer
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
+    filterset_fields = ['location', 'price']  
+    search_fields = ['title', 'description']  
+    ordering_fields = ['created_at', 'price'] 
 
 class ListingDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Listing.objects.all()
