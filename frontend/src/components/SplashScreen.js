88f46437d3974c3c1f72d06d4b6a334930cf284b
projectaccount007 <103994837+projectaccount007@users.noreply.github.com>
2025-04-09 19:32:51 +0530

    splash screen responsive design trial

--- frontend/src/components/SplashScreen.js
+++ frontend/src/components/SplashScreen.js
@@ -10,26 +10,23 @@ const SplashScreen = () => {
       </header>
 
       <div className="content">
-        {/* Logo Section */}
-        
-        {/* Illustration */}
-        
-
-        {/* Bottom Section */}
+        {/* Bottom Section - includes image + text */}
         <div className="bottom-section">
-         <div className="illustration-container">
-          <img
+          <div className="illustration-container">
+            <img
               src="/illustration.avif"
               alt="Illustration"
               className="illustration"
             />
-         </div>
-          <h2>Everything you <br /> need in go</h2>
-          <p>
-            Easily buy & sell products, find jobs, and discover local services
-            all in one app!
-          </p>
-          <button className="get-started-btn">Get started</button>
+          </div>
+          <div className="text-container">
+            <h2>Everything you <br /> need in one go</h2>
+            <p>
+              Easily buy & sell products, find jobs, and discover local services
+              all in one app!
+            </p>
+            <button className="get-started-btn">Get started</button>
+          </div>
         </div>
       </div>
     </div>
