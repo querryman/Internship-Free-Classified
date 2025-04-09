0df76b38b75500e871750549a89ba36f3b2dfc4d
AMAL <amaljoseph126@gmail.com>
2025-04-09 19:10:28 +0530

    Landing page

--- frontend/src/components/SplashScreen.js
+++ frontend/src/components/SplashScreen.js
@@ -4,30 +4,28 @@ import "./SplashScreen.css";
 const SplashScreen = () => {
   return (
     <div className="app-container">
-      {/* Header */}
       <header className="header">
         <h1 className="logo-text">TRADEX</h1>
       </header>
-
-      <div className="content">
-        {/* Bottom Section - includes image + text */}
-        <div className="bottom-section">
-          <div className="illustration-container">
-            <img
-              src="/illustration.avif"
-              alt="Illustration"
-              className="illustration"
-            />
-          </div>
-          <div className="text-container">
-            <h2>Everything you <br /> need in one go</h2>
-            <p>
-              Easily buy & sell products, find jobs, and discover local services
-              all in one app!
-            </p>
-            <button className="get-started-btn">Get started</button>
-          </div>
+      <div className="illustration-container">
+          <img
+            src="/illustration.avif"
+            alt="Illustration"
+            className="illustrations"
+          />
         </div>
+
+      
+
+      {/* Bottom Section */}
+      <div className="bottom-section">
+      
+        <h2>Everything you need in go</h2>
+        <p>
+          Easily buy & sell products, find jobs, and discover local services
+          all in one app!
+        </p>
+        <button className="get-started-btn">Get started</button>
       </div>
     </div>
   );
