8a710aaa1ed2674c206e8ee644f5beffff417c5e
projectaccount007 <103994837+projectaccount007@users.noreply.github.com>
2025-04-29 18:51:44 +0530

    setting base

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Internship-Free-Classified/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
